2dc7892033cfb001d00b766694e9d2e27d178c0e
Joseph McCallum-Nattrass <jmn@jmnetwork.uk>
2022-03-09 20:55:52 +0000

    Finished basic functionality, need to add input methods

--- cloudflare-ddns.go
+++ cloudflare-ddns.go
@@ -7,6 +7,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/cloudflare/cloudflare-go"
 )
@@ -50,12 +51,24 @@ func SetAPIToken(token string) {
 	os.Setenv("CLOUDFLARE_API_TOKEN", token)
 }
 
+//GetZoneID gets the zone ID set in the system variables
 func GetZoneID() {
-	apiToken = os.Getenv("CLOUDFLARE_ZONE_ID")
+	zoneId = os.Getenv("CLOUDFLARE_ZONE_ID")
 }
 
+//SetZoneID takes in a Zone ID and sets it as a system variable
+func SetZoneID(zoneID string) {
+	os.Setenv("CLOUDFLARE_ZONE_ID", zoneID)
+}
+
+//GetRecordID gets the record ID set in the system variables
 func GetRecordID() {
-	apiToken = os.Getenv("CLOUDFLARE_RECORD_ID")
+	recordId = os.Getenv("CLOUDFLARE_RECORD_ID")
+}
+
+//SetRecordID sets the record ID to the provided parameter
+func SetRecordID(recordID string) {
+	os.Setenv("CLOUDFLARE_ZONE_ID", recordID)
 }
 
 //Main Functions
@@ -80,10 +93,10 @@ func fetchIP() error {
 }
 
 //Compare IP will check if the current IP matches the DNS records and return true if it does not
-func CompareIPToRecord(zoneID, recordID string) (cloudflare.DNSRecord, bool) {
+func CompareIPToRecord() (cloudflare.DNSRecord, bool) {
 	var dnsRecord cloudflare.DNSRecord
 	//Make an API request to cloudflare to get the IP address in the A record
-	dnsRecord, err := api.DNSRecord(ctx, zoneID, recordID)
+	dnsRecord, err := api.DNSRecord(ctx, zoneId, recordId)
 	if err != nil {
 		log.Println("Could not fetch DNS Record! Is the API key valid? Does it have the right permissions?")
 		return dnsRecord, false
@@ -98,25 +111,45 @@ func CompareIPToRecord(zoneID, recordID string) (cloudflare.DNSRecord, bool) {
 }
 
 //UpdateDNSRecords updates an A record with the current IP
-func UpdateDNSRecord(oldRecord cloudflare.DNSRecord, zoneID, recordID string) error {
+func UpdateDNSRecord(oldRecord cloudflare.DNSRecord) error {
 	//Change the old record IP and send it
 	oldRecord.Content = ip.Query
-	err := api.UpdateDNSRecord(ctx, zoneID, recordID, oldRecord)
+	err := api.UpdateDNSRecord(ctx, zoneId, recordId, oldRecord)
 	if err != nil {
 		return err
 	}
 	return nil
 }
 
+func RecordUpdateWorker() {
+	for {
+		//Fetch the current IP
+		fetchIP()
+		//Compare to the current record
+		oldRecord, changed := CompareIPToRecord()
+		if changed {
+			UpdateDNSRecord(oldRecord)
+		}
+		time.Sleep(time.Minute)
+	}
+}
+
 func main() {
-	//Startup options
-
 	//Set logging location
+	f, err := os.OpenFile("cloudflare-ddns.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatalf("error opening file: %v", err)
+	}
+	defer f.Close()
 
+	log.SetOutput(f)
 	//Declare the API
 	GetAPIToken()
 	api, _ = cloudflare.NewWithAPIToken(apiToken)
 
 	//Set the context
 	ctx = context.Background()
+
+	//Start the worker
+	RecordUpdateWorker()
 }
